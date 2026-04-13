a89f4df23e04bc17cdbe8cfbd7be83e2d34c6933
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2026-04-13 16:50:48 +0200

    Performance and feature improvements

--- src/Tui.hs
+++ src/Tui.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE Strict #-}
+
 module Tui (startTui) where
 
 import Lens.Micro ((^.))
 import Lens.Micro.TH
+import Lens.Micro.Mtl ((%=), (.=), use)
 import Control.Monad (void)
 #if !(MIN_VERSION_base(4,11,0))
 import Data.Monoid
 #endif
 import Data.Maybe
+import Text.Printf
 import qualified Graphics.Vty as V
 
 import qualified Brick.Main as M
@@ -38,7 +43,7 @@ import Data.Map (Map, lookup)
 import Sass
 
 type RowData = (Integer, (Instruction, Map String String))
-data InstrData = InstrData (Maybe RowData)
+type InstrData = Maybe RowData
 
 data Row 
   = TextRow String String String String String String
@@ -46,10 +51,27 @@ data Row
   | HalfWidthText String String
   | FullWidthText String
 
+
+allMetrics :: [String]
+allMetrics = 
+    [ "Instructions Executed"
+    , "Predicated-On Thread Instructions Executed"
+    , "Thread Instructions Executed"
+    , "Warp Stall Sampling (All Samples)"
+    , "Warp Stall Sampling (Not-issued Samples)"
+    ]
+
+
 data AppState =
-    AppState { _tabularList :: L.List () Row
+    AppState { _rawList :: [(InstrData, InstrData)]
+             , _tabularList :: L.List () Row
              , _fn1 :: String
              , _fn2 :: String
+             , _showRelativeMetrics :: Bool
+             , _hideRows :: Bool
+             , _metricIndex :: Int
+             , _totalMetricValue1 :: Integer
+             , _totalMetricValue2 :: Integer
              }
 
 
@@ -65,28 +87,70 @@ drawUI s = [ui]
                 Just i -> show (i + 1)
         box = B.borderWithLabel lbl $
               hLimit totalWidth $
-              listDrawElement False (HalfWidthText (s^.fn1) (s^.fn2)) <=>
-              listDrawElement False headerRow <=>
+              listDrawElement s False (HalfWidthText (s^.fn1) (s^.fn2)) <=>
+              listDrawElement s False headerRow <=>
               hBorder <=>
-              L.renderList (listDrawElement) True l
+              L.renderList (listDrawElement s) True l <=>
+              hBorder <=>
+              str ("[M]etric: " ++ (allMetrics !! (s^.metricIndex))) <=>
+              str ("[R]elative metrics: " ++ (if s^.showRelativeMetrics then "Yes" else "No")) <=>
+              str ("[H]ide rows: " ++ (if s^.hideRows then "Yes" else "No"))
         ui = C.vCenter $ vBox [ C.hCenter box ]
+
+toggleRelativeMetrics :: T.EventM () AppState ()
+toggleRelativeMetrics = showRelativeMetrics %= not
+
+toggleHideRows :: T.EventM () AppState ()
+toggleHideRows = do
+    hideRows %= not
+    oldList <- use tabularList
+    let oldIndex = case (L.listSelectedElement oldList) of
+            (Just (0, _)) -> 0
+            (Just (n, (InstrRow a b))) -> n
+            _ -> 0
+    let clipRow r = case r of
+            (InstrRow a b) -> Just (fmap fst $ a, fmap fst $ b)
+            _ -> Nothing
+    let oldFixedElement = clipRow ((L.listElements oldList) `Vec.unsafeIndex` oldIndex)
+    newHideRows <- use hideRows
+    theRawList <- use rawList
+    tabularList .= (L.list () (Vec.fromList (map renderRow . (if newHideRows then (compressRows 2) else noCompressRows) $ theRawList)) 1)
+    newList <- use tabularList
+    case L.listFindFirst ((== oldFixedElement) . (\e -> clipRow e)) newList of
+        Just (x, _) -> tabularList %= (L.listMoveTo x)
+        Nothing -> return ()
+
+changeMetric :: Bool -> T.EventM () AppState ()
+changeMetric b = do
+    metricIndex %= (\x -> (x + (length allMetrics) + (if b then 1 else -1)) `rem` (length allMetrics))
+    newMetricIndex <- use metricIndex
+    rl <- use rawList
+    totalMetricValue1 .= (sum . map (read . fromJust . Data.Map.lookup (allMetrics !! newMetricIndex) . snd . snd) . catMaybes . map fst $ rl)
+    totalMetricValue2 .= (sum . map (read . fromJust . Data.Map.lookup (allMetrics !! newMetricIndex) . snd . snd) . catMaybes . map snd $ rl)
+    return () 
 
 appEvent :: T.BrickEvent () e -> T.EventM () AppState ()
 appEvent (T.VtyEvent e) =
     case e of
         V.EvKey V.KEsc [] -> M.halt
+        V.EvKey (V.KChar 'r') [] -> toggleRelativeMetrics
+        V.EvKey (V.KChar 'R') [] -> toggleRelativeMetrics
+        V.EvKey (V.KChar 'h') [] -> toggleHideRows
+        V.EvKey (V.KChar 'H') [] -> toggleHideRows
+        V.EvKey (V.KChar 'm') [] -> changeMetric True
+        V.EvKey (V.KChar 'M') [] -> changeMetric False
         ev -> T.zoom tabularList $ L.handleListEvent ev
 appEvent _ = return ()
 
 makeColumns as ws ts = Table.alignColumns (intersperse Table.AlignLeft as) (intersperse 1 ws) (intersperse (str " ") ts)
 
-listDrawElement :: Bool -> Row -> Widget ()
-listDrawElement _ (TextRow a b c d e f) =
+listDrawElement :: AppState -> Bool -> Row -> Widget ()
+listDrawElement st _ (TextRow a b c d e f) =
     hLimit totalWidth $
     hBox $
     makeColumns (columnHeaderAlignments ++ columnHeaderAlignments) (perSourceColumnWidths ++ perSourceColumnWidths) [str a, str b, str c, str d, str e, str f]
-listDrawElement sel (InstrRow (InstrData a) (InstrData b)) =
-    let ws = [str (renderLineNr a), str (renderInstr a), str (renderProfileData a), str (renderLineNr b), str (renderInstr b), str (renderProfileData b)]
+listDrawElement st sel (InstrRow a b) =
+    let ws = [str (renderLineNr a), str (renderInstr a), str (renderProfileData (allMetrics !! (st^.metricIndex)) (st^.totalMetricValue1) (st^.showRelativeMetrics) a), str (renderLineNr b), str (renderInstr b), str (renderProfileData (allMetrics !! (st^.metricIndex)) (st^.totalMetricValue1) (st^.showRelativeMetrics) b)]
         maybeSelect es = selectCell <$> zip [0..] es
         selectCell (i, w) = case (sel, isNothing a, isNothing b, i) of
           (True, False, False, _) -> withDefAttr selectedCellAttr w
@@ -105,17 +169,17 @@ listDrawElement sel (InstrRow (InstrData a) (InstrData b)) =
        hBox $
        maybeSelect $
        makeColumns (columnAlignments ++ columnAlignments) (perSourceColumnWidths ++ perSourceColumnWidths) ws
-listDrawElement sel (FullWidthText s) =
+listDrawElement st sel (FullWidthText s) =
     hLimit totalWidth $
     hBox $
     maybeSelect $
-    makeColumns [Table.AlignCenter] [totalWidth] [str s]
+    makeColumns [Table.AlignCenter] [totalWidth + 2 * (length perSourceColumnWidths)] [str s]
     where
         maybeSelect es = selectCell <$> zip [0..] es
         selectCell (_, w) = case (sel) of
             True -> withDefAttr annotationSelectedCellAttr w
             False -> withDefAttr annotationCellAttr w
-listDrawElement _ (HalfWidthText a b) =
+listDrawElement st _ (HalfWidthText a b) =
     hLimit totalWidth $
     hBox $
     makeColumns [Table.AlignLeft, Table.AlignLeft] [l, l] [str a, str b]
@@ -155,11 +219,14 @@ theMap = A.attrMap V.defAttr
     , (loneSelectedCellAttr,          V.white `on` V.green)
     ]
 
+sourceCellWidth :: Int
+sourceCellWidth = 50
+
 perSourceColumnWidths :: [Int]
-perSourceColumnWidths = [6, 50, 10]
+perSourceColumnWidths = [6, sourceCellWidth, 8]
 
 totalWidth :: Int
-totalWidth = 2 * (sum perSourceColumnWidths)
+totalWidth = 2 * (sum perSourceColumnWidths) + 2 * (length perSourceColumnWidths) - 1
 
 headerRow :: Row
 headerRow = TextRow "Line" "Source" "Exec." "Line" "Source" "Exec."
@@ -179,23 +246,30 @@ renderInstr :: Maybe RowData -> String
 renderInstr (Just (_, (x, _))) = show x
 renderInstr Nothing = " "
 
-renderProfileData :: Maybe RowData -> String
-renderProfileData (Just (_, (_, m))) = fromJust . Data.Map.lookup "Instructions Executed" $ m
-renderProfileData Nothing = " "
+renderProfileData :: String -> Integer -> Bool -> Maybe RowData -> String
+renderProfileData n t r (Just (_, (_, m)))
+    | r = printf "%.2f%%" (100.0 * ((fromInteger raw) / (fromInteger t)) :: Float)
+    | raw > 0 = show raw
+    | otherwise = " "
+    where
+        raw = read . fromJust . Data.Map.lookup n $ m :: Integer
+renderProfileData _ _ _ Nothing = " "
 
 renderRow :: (Either (Maybe RowData, Maybe RowData) Integer) -> Row
-renderRow (Left (a, b)) = InstrRow (InstrData a) (InstrData b)
+renderRow (Left (a, b)) = InstrRow a b
 renderRow (Right n) = FullWidthText ("Hiding " ++ (show n) ++ " rows")
 
-cellToBeKept :: RowData -> Bool
-cellToBeKept (_, (_, m)) = nExec > 0
-    where
-        nExec = (read . fromJust . Data.Map.lookup "Instructions Executed" $ m) :: Integer
+getInstExec :: RowData -> Integer
+getInstExec (_, (_, m)) = read . fromJust . Data.Map.lookup "Instructions Executed" $ m
 
 rowToBeKept :: (Maybe RowData, Maybe RowData) -> Bool
-rowToBeKept (Just _, Just _) = False
-rowToBeKept (Just a, Nothing) = cellToBeKept a
-rowToBeKept (Nothing, Just a) = cellToBeKept a
+rowToBeKept (Just a, Just b) = frac <= 0.95 || frac >= 1.05
+    where
+        iExA = (getInstExec a)
+        iExB = (getInstExec b)
+        frac = ((fromInteger iExA) :: Float) / ((fromInteger iExB) :: Float)
+rowToBeKept (Just a, Nothing) = (getInstExec a) > 0
+rowToBeKept (Nothing, Just a) = (getInstExec a) > 0
 rowToBeKept _ = True
 
 compressRows :: Integer -> [(Maybe RowData, Maybe RowData)] -> [Either (Maybe RowData, Maybe RowData) Integer]
@@ -219,6 +293,10 @@ compressRows m q = go True q []
                 midLen = toInteger . length $ cmid
 
 
+noCompressRows :: [(Maybe RowData, Maybe RowData)] -> [Either (Maybe RowData, Maybe RowData) Integer]
+noCompressRows = map Left
+
+
 startTui :: String -> String -> [(Maybe RowData, Maybe RowData)] -> IO ()
 startTui fn1 fn2 is = void $ M.defaultMain theApp initialState
   where
@@ -230,8 +308,15 @@ startTui fn1 fn2 is = void $ M.defaultMain theApp initialState
             }
     compressedRows = compressRows 2 is
     initialRows = map renderRow compressedRows
-    initialState =
-      AppState { _tabularList = L.list () (Vec.fromList initialRows) 1
+    rawInitialState =
+      AppState { _rawList = is
+               , _tabularList = L.list () (Vec.fromList initialRows) 1
                , _fn1=fn1
                , _fn2=fn2
+               , _showRelativeMetrics=False
+               , _hideRows=True
+               , _metricIndex=0
+               , _totalMetricValue1=(sum . map (read . fromJust . Data.Map.lookup (allMetrics !! 0) . snd . snd) . catMaybes . map fst $ is)
+               , _totalMetricValue2=(sum . map (read . fromJust . Data.Map.lookup (allMetrics !! 0) . snd . snd) . catMaybes . map snd $ is)
                }
+    initialState = rawInitialState
